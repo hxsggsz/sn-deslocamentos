ef5ffa41eed9f93b54515b06fcc836ad5636880d
hxsggsz <vt.hugo.2021@gmail.com>
2023-06-28 22:20:10 -0300

    fix: build error on deploy V1.0

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { IUF } from '@/utils/types'
-import { GetStaticProps } from 'next/types'
+import { GetServerSideProps } from 'next/types'
 import { SignUp } from '@/components/auth/client/signup'
 
 export default function Signup({ states }: IUF) {
@@ -8,7 +8,7 @@ export default function Signup({ states }: IUF) {
 }
 
 // os UFs são os mesmos e nunca irão mudar e não dependem de outros inputs para serem renderizados, por isso estou passando ele via getStaticProps que irá rodar esse código durante a build do site
-export const getStaticProps: GetStaticProps<{ states: IUF }> = async () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   const statesResponse = await axios.get(
     'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome',
   )
