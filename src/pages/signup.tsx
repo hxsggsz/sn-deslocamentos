32334dcb739601bdcd1f158617759df6ecc58929
hxsggsz <vt.hugo.2021@gmail.com>
2023-06-28 22:45:10 -0300

    fix build error V1.6

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,24 +1,6 @@
-import axios from 'axios'
-import { IUF } from '@/utils/types'
-import { GetStaticProps } from 'next/types'
+import states from '../../states.json'
 import { SignUp } from '@/components/auth/client/signup'
 
-export default function Signup({ states }: IUF) {
+export default function Signup() {
   return <SignUp states={states} />
-}
-
-// os UFs são os mesmos e nunca irão mudar e não dependem de outros inputs para serem renderizados, por isso estou passando ele via getStaticProps que irá rodar esse código durante a build do site
-export const getStaticProps: GetStaticProps = async () => {
-  let states = null
-  try {
-    const { data } = await axios.get(
-      'https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome',
-    )
-    if (data) {
-      states = data
-    }
-  } catch (error) {
-    console.log(error)
-  }
-  return { props: { states } }
 }
